763b3b193d96409b71d18af5d7192e25a1b8258a
Jason Gravell <jason.gravell@cloudconstruct.com>
2022-02-07 15:23:56 -0500

    Azure static web app

--- gulpfile.js
+++ gulpfile.js
@@ -1,71 +1,87 @@
 var gulp = require('gulp'),
-    webserver = require("gulp-webserver"),
-    sourcemaps = require("gulp-sourcemaps"),
-    concat = require("gulp-concat"),
-    sass = require("gulp-sass"),
-    autoprefixer = require('gulp-autoprefixer'),
-		cleanCSS = require("gulp-clean-css");
-		cachebust = require('gulp-cache-bust');
+  webserver = require('gulp-webserver'),
+  sourcemaps = require('gulp-sourcemaps'),
+  concat = require('gulp-concat'),
+  sass = require('gulp-sass')(require('sass')),
+  autoprefixer = require('gulp-autoprefixer'),
+  cleanCSS = require('gulp-clean-css'),
+  cachebust = require('gulp-cache-bust');
 
 // JS
-gulp.task('js', gulp.parallel(function () {
-    // gulp.src(['./src/js/vendor/**/*.*'])
-    //     .pipe(gulp.dest('./js/vendor/'));
-    return gulp.src(['./Assets/Scripts/site.js'])
-				.pipe(concat('main.js'))
-				.pipe(cachebust({
-					type: 'timestamp'
-				}))
-        .pipe(gulp.dest('./Assets/dist/js/'));
-}));
-
-
-// sass task
-gulp.task('sass', gulp.parallel(function() {
-    return gulp.src(['./Assets/Styles/general.scss'])
-        .pipe(sourcemaps.init())
-            .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-				.pipe(sourcemaps.write('.'))
-				.pipe(cachebust({
-					type: 'timestamp'
-				}))
-        .pipe(gulp.dest('./Assets/dist/css/'));
-}));
-
-// Minify CSS
-gulp.task('minify-css', gulp.parallel(() => {
-    return gulp.src('./Assets/dist/css/*.css')
-      .pipe(sourcemaps.init())
-      .pipe(cleanCSS({compatibility: 'ie8'}))
-      .pipe(sourcemaps.write())
-      .pipe(gulp.dest('.'));
-  }));
-
-// local server for dev
-gulp.task('serve', function() {
-  gulp.src('./')
-    .pipe(webserver({
-      port:'9090',
-      livereload: true,
-      open: true
-    }));
+gulp.task('js', function () {
+  return gulp
+    .src(['./Assets/Scripts/site.js'])
+    .pipe(concat('main.js'))
+    .pipe(
+      cachebust({
+        type: 'timestamp',
+      })
+    )
+    .pipe(gulp.dest('./dist/Assets/js/'));
 });
 
+// sass task
+gulp.task('sass', function () {
+  return gulp
+    .src(['./Assets/Styles/general.scss'])
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(
+      autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false,
+      })
+    )
+    .pipe(sourcemaps.write('.'))
+    .pipe(
+      cachebust({
+        type: 'timestamp',
+      })
+    )
+    .pipe(gulp.dest('./dist/Assets/css/'));
+});
+
+// Minify CSS
+gulp.task('minify-css', () => {
+  return gulp
+    .src('./dist/Assets/css/*.css')
+    .pipe(sourcemaps.init())
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./dist/Assets/css'));
+});
+
+// Asset copy
+gulp.task('Assets', function () {
+  return gulp
+    .src('./Assets/@(Icons|Images)/**/*.*')
+    .pipe(gulp.dest('./dist/Assets/'));
+});
+
+// local server for dev
+gulp.task('serve', function () {
+  gulp.src('./dist').pipe(
+    webserver({
+      port: '9090',
+      livereload: true,
+      open: true,
+      fallback: 'index.html',
+    })
+  );
+});
 
 // Watchers
-gulp.task('watch', gulp.series(function() {
-    gulp.watch([
-        './Assets/Styles/*.scss'
-    ], gulp.series('sass', 'minify-css'));
-    gulp.watch([
-        './Assets/scripts/*.js'
-    ],  gulp.series('js'));    
-}));
+gulp.task(
+  'watch',
+  gulp.series(function () {
+    gulp.watch(['./Assets/Styles/*.scss'], gulp.series('sass', 'minify-css'));
+    gulp.watch(['./Assets/scripts/*.js'], gulp.series('js'));
+  })
+);
 
+gulp.task(
+  'build',
+  gulp.parallel(['js', 'Assets', gulp.series(['sass', 'minify-css'])])
+);
 
-
-gulp.task('default', gulp.series(['sass', 'js', 'minify-css'], gulp.parallel(['serve', 'watch'])));
+gulp.task('default', gulp.series('build', gulp.parallel(['serve', 'watch'])));
